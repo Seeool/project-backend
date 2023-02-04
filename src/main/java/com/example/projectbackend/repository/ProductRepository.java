c81e3e218a3043809e774a196755f66ccf2f1042
Seeool <news4214@protonmail.com>
2023-02-04 09:57:25 +0900

    23/01/31 쿼리문 수정

--- src/main/java/com/example/projectbackend/repository/ProductRepository.java
+++ src/main/java/com/example/projectbackend/repository/ProductRepository.java
@@ -16,27 +16,40 @@ import java.util.Objects;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductSearch {
+    @EntityGraph(attributePaths = "imageSet")
     List<Product> findFirst6ByOrderByRegDateDesc();
     //등록 최신순으로 6상품 찾기
-    
+
+    @EntityGraph(attributePaths = "imageSet")
     List<Product> findByCategoryIs(int category);
     //특정 카테고리 상품 찾기
-    
+
+    @EntityGraph(attributePaths = "imageSet")
     List<Product> findByDiscountIs(boolean discount);
     //할인 중인 상품 찾기
-    
+
+    @EntityGraph(attributePaths = "imageSet")
     List<Product> findByDiscountIsTrueAndCategoryIs(int category);
     //할인 중인 상품 중 특정 카테고리 상품 찾기
 
-    @Query("select p, pi, avg(coalesce(r.grade,0)) from Product p left join ProductImage pi on (pi.product = p and pi.ord = (select min(pii.ord) from ProductImage pii where pii.product = p)) left join Review r on r.product = p group by p order by avg(coalesce(r.grade,0)) desc, p.salesVolume desc limit 6")
+    @Query("select p, pi, avg(coalesce(r.grade,0)) from Product p " +
+            "left join ProductImage pi " +
+            "on (pi.product = p and pi.ord = (select min(pii.ord) " +
+            "from ProductImage pii where pii.product = p)) " +
+            "left join Review r " +
+            "on r.product = p " +
+            "group by p " +
+            "order by avg(coalesce(r.grade,0)) desc, p.salesVolume desc " +
+            "limit 6")
     List<Object[]> findFirst6ByOrderByReviewAvgDesc();
 
     List<Product> findFirst12ByOrderBySalesVolumeDesc();
     //판매량(인기)가 많은 순으로 12상품 찾기
 
 
-    @Query("select p, pi, avg(coalesce(r.grade,0)), count(distinct r) from Product p left join ProductImage pi on pi.product = p left join Review r on r.product = p where p.pid = :pid group by pi")
-    List<Object[]> findOneWithReviewAvg(@Param("pid") Long pid); //모든 이미지 다 가져오기 group by pi임
-    //Object[]는 Optional이 아니라 List로,, 모든 이미지 다 가져오기
+    @Query("select p, pi, avg(coalesce(r.grade,0)), count(distinct r) from Product p " +
+            "left join ProductImage pi on pi.product = p left join Review r on r.product = p " +
+            "where p.pid = :pid group by pi")
+    List<Object[]> findOneWithReviewAvg(@Param("pid") Long pid);
 
 }
